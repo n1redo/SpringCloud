def01d53dae2273269027f9bfb9954a1bfef44c9
n1redo <167955761+n1redo@users.noreply.github.com>
2024-06-03 18:24:10 +0800

    2024-06-03

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -0,0 +1,18 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * 主启动类
+ *
+ * @author nhh
+ */
+@SpringBootApplication
+@MapperScan("com.atguigu.cloud.mapper")
+public class Main8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8001.class, args);
+    }
+}
