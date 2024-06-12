b9120016357bf09ddd1d5ec82a916c47e1b517ea
n1redo <167955761+n1redo@users.noreply.github.com>
2024-06-12 11:01:55 +0800

    2024-06-12

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud.controller;
 
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
 import com.atguigu.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,41 +21,42 @@ import java.util.List;
 @RestController
 @Slf4j
 @RequestMapping("/pay")
-@Tag(name = "支付微服务模块",description = "支付CRUD")
+@Tag(name = "支付微服务模块", description = "支付CRUD")
 public class PayController {
     @Resource
     private PayService payService;
 
     @PostMapping("/add")
-    @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay) {
+    @Operation(summary = "新增", description = "新增支付流水方法,json串做参数")
+    public ResultData<String> addPay(@RequestBody Pay pay) {
         int add = payService.add(pay);
-        return "成功插入记录,返回值" + add;
+        return ResultData.success("成功插入记录，返回值：" + add);
     }
 
     @DeleteMapping("/del/{id}")
-    @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    @Operation(summary = "删除", description = "删除支付流水方法")
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        int delete = payService.delete(id);
+        return ResultData.success(delete);
     }
 
     @PutMapping("/update")
-    @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO payDTO) {
+    @Operation(summary = "修改", description = "修改支付流水方法")
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO) {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
         int update = payService.update(pay);
-        return "成功修改记录,返回值" + update;
+        return ResultData.success("成功修改记录,返回值" + update);
     }
 
     @GetMapping("/get/{id}")
-    @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
+    @Operation(summary = "按照ID查流水", description = "查询支付流水方法")
     public Pay getById(@PathVariable("id") Integer id) {
         return payService.getById(id);
     }
 
     @PostMapping("/getAll")
-    @Operation(summary = "查询",description = "查询所有支付流水方法")
+    @Operation(summary = "查询", description = "查询所有支付流水方法")
     public List<Pay> getAll() {
         return payService.getAll();
     }
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/exp/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.atguigu.cloud.exp;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 全局异常处理
+ *
+ * @author nhh
+ */
+@Slf4j
+public class GlobalExceptionHandler {
+}
