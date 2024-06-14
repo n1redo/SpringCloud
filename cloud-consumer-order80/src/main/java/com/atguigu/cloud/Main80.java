8e45f712806a74ffc9255fac443968adbd57b0a0
n1redo <167955761+n1redo@users.noreply.github.com>
2024-06-14 19:49:22 +0800

    2024-06-14

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/Main80.java
@@ -0,0 +1,16 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动类
+ *
+ * @author nhh
+ */
+@SpringBootApplication
+public class Main80 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main80.class, args);
+    }
+}
